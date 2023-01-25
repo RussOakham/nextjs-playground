e1dadb7b3b7fcf1b11ca1ed74d7b559c6d224ed7
Russell Oakham <russell.oakham@and.digital>
2023-01-25 22:13:32 +0000

    Nav - added conditional rendering for if mobile or not

--- src/components/layout/navigation/MainNavigation.tsx
+++ src/components/layout/navigation/MainNavigation.tsx
@@ -1,5 +1,7 @@
 import { Disclosure } from '@headlessui/react'
 
+import useWindowDimension from '@/hooks/useWindowDimension'
+
 import CompanyLogo from './CompanyLogo'
 import NotificationButton from './NotificationButton'
 import ProfileButton from './ProfileButton'
@@ -21,16 +23,22 @@ type PropTypes = {
 const MainNavigation = ({ className }: PropTypes) => {
     const displayNotificationButton = false
 
+    const { width } = useWindowDimension()
+
     return (
         <Disclosure as="nav" className={`${className} bg-gray-800`}>
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                         <div className="relative flex h-16 items-center justify-between">
-                            <MobileHamburger open={open} />
+                            {width < 640 ? (
+                                <MobileHamburger open={open} />
+                            ) : null}
                             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                                 <CompanyLogo />
-                                <NavMenu navigation={navigation} />
+                                {width >= 640 ? (
+                                    <NavMenu navigation={navigation} />
+                                ) : null}
                             </div>
                             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 {displayNotificationButton && (
@@ -42,7 +50,9 @@ const MainNavigation = ({ className }: PropTypes) => {
                         </div>
                     </div>
 
-                    <MobileMenuNav navigation={navigation} />
+                    {width < 640 ? (
+                        <MobileMenuNav navigation={navigation} />
+                    ) : null}
                 </>
             )}
         </Disclosure>
