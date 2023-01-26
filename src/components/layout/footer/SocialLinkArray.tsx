00d26eebab409463d077a03ff4a70e8ee2a6b220
Russell Oakham <russell.oakham@and.digital>
2023-01-26 00:01:52 +0000

    Footer - added react-icons and updated footer icons to use them

--- src/components/layout/footer/SocialLinkArray.tsx
+++ src/components/layout/footer/SocialLinkArray.tsx
@@ -1,9 +1,11 @@
+import { useMemo } from 'react'
 import Link from 'next/link'
+import { IconContext, IconType } from 'react-icons'
 
 type Item = {
     name: string
     href: string
-    icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element
+    image: IconType | JSX.Element
 }
 
 type PropTypes = {
@@ -11,18 +13,24 @@ type PropTypes = {
 }
 
 const SocialLinkArray = ({ navigation }: PropTypes) => {
+    const iconStyles = useMemo(() => ({ className: 'h-6 w-6' }), [])
+
     return (
         <div className="flex justify-center space-x-6 md:order-2">
-            {navigation.map((item) => (
-                <Link
-                    key={item.name}
-                    href={item.href}
-                    className="text-white hover:text-gray-500"
-                >
-                    <span className="sr-only">{item.name}</span>
-                    <item.icon className="h-6 w-6" aria-hidden="true" />
-                </Link>
-            ))}
+            <IconContext.Provider value={iconStyles}>
+                {navigation.map((item) => (
+                    <Link
+                        key={item.name}
+                        href={item.href}
+                        className="text-white hover:text-gray-500"
+                    >
+                        <>
+                            <span className="sr-only">{item.name}</span>
+                            {item.image}
+                        </>
+                    </Link>
+                ))}
+            </IconContext.Provider>
         </div>
     )
 }
