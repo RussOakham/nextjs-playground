e872f23c3a2655d0909b4ef9c926898c1f09e608
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-01-26 21:57:43 +0000

    Navlinks - further fixes to active link recognition mobile

--- src/components/layout/navigation/mobile/MobileNavLink.tsx
+++ src/components/layout/navigation/mobile/MobileNavLink.tsx
@@ -23,10 +23,8 @@ const MobileNavLink = ({ item }: PropTypes) => {
                     router.pathname === item.href
                         ? 'bg-gray-900 text-white'
                         : 'text-gray-300 hover:bg-gray-700 hover:text-white'
-                } px-3 py-2 rounded-md text-sm font-medium`}
-                aria-current={
-                    router.pathname === item.href ? 'page' : undefined
-                }
+                } block px-3 py-2 rounded-md text-base font-medium`}
+                aria-current={item.current ? 'page' : undefined}
             >
                 {item.name}
             </Disclosure.Button>
