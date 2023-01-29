5134167f024f9735d004d0b94ca48adb5d7b8e18
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-01-29 14:31:46 +0000

    Auth styling updates

--- src/components/UX/buttons/MainButton.tsx
+++ src/components/UX/buttons/MainButton.tsx
@@ -7,7 +7,7 @@ const MainButton = ({ text, type }: MainButtonProps) => {
     return (
         <button
             type={type}
-            className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 active:bg-indigo-900"
         >
             {text}
         </button>
--- src/components/forms/inputs/TextInput.tsx
+++ src/components/forms/inputs/TextInput.tsx
@@ -35,7 +35,7 @@ export const TextInput = ({
             placeholder={placeholder}
             autoComplete={autoComplete || 'off'}
             {...register(id)}
-            className={`${className} block w-full px-3 py-2 mt-1 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm`}
+            className={`${className} block w-full px-3 py-2 mt-1 placeholder-gray-400 border border-gray-300 rounded-md shadow-sm appearance-none focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm invalid:text-red-500`}
             {...props}
         />
     )
