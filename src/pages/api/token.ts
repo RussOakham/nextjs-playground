129bfcb82a02dca8fdee133f556b453b228ac2ac
RussOakham <73036915+RussOakham@users.noreply.github.com>
2023-02-01 22:20:28 +0000

    token - updated logged out return

--- src/pages/api/token.ts
+++ src/pages/api/token.ts
@@ -5,5 +5,11 @@ const secret = process.env.SECRET
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const token = await getToken({ req, secret })
+
+    if (!token) {
+        res.status(401).send('Unauthorized')
+        return
+    }
+
     res.send(JSON.stringify(token, undefined, 2))
 }
